1710232f7f1fdfa09184c7b960f2a769e9d6a3f0
CubeAndy <a072682@gmail.com>
2024-10-15 22:07:08 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { ViteEjsPlugin } from 'vite-plugin-ejs';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 import { glob } from 'glob';
+
 import liveReload from 'vite-plugin-live-reload';
 
 function moveOutputPlugin() {
@@ -17,54 +18,28 @@ function moveOutputPlugin() {
           bundle[fileName].fileName = newFileName;
         }
       }
-      if (bundle['pages/index.html']) {
-        bundle['pages/index.html'].fileName = 'index.html';
-      }
     },
   };
 }
 
-function htmlPlugin(base) {
-  const baseWithoutTrailingSlash = base.endsWith('/') ? base.slice(0, -1) : base;
-  
-  return {
-    name: 'html-transform',
-    transformIndexHtml(html, { filename }) {
-      // 處理資源路徑，避免重複的基本路徑
-      html = html.replace(
-        new RegExp(`(src|href)="${baseWithoutTrailingSlash}/${baseWithoutTrailingSlash}/`, 'g'),
-        `$1="${baseWithoutTrailingSlash}/`
-      );
-      
-      // 處理內部頁面鏈接，包括相對路徑
-      html = html.replace(/(href=")(?:\.\.\/pages\/|pages\/)(.*?\.html)/g, (match, p1, p2) => {
-        const pageName = p2.replace('.html', '');
-        return `${p1}${base}${pageName}`;
-      });
-      
-      // 處理圖片路徑，移除 "../" 並直接指向 assets 目錄
-      html = html.replace(/(src=")(\.\.\/assets\/.*?\.(png|jpg|jpeg|gif|svg))/g, (match, p1, p2) => {
-        return `${p1}${base}assets/${p2.split('/').pop()}`;
-      });
-      
-      return html;
-    }
-  }
-}
-
 export default defineConfig({
-  base: '/web-layout-training-vite/',
+  // base 的寫法：
+  // base: '/Repository 的名稱/'
+  base: '/Come-Buy-team-work/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
     moveOutputPlugin(),
-    htmlPlugin('/web-layout-training-vite/'),
   ],
+  server: {
+    // 啟動 server 時預設開啟的頁面
+    open: 'page/index.html',
+  },
   build: {
     rollupOptions: {
       input: Object.fromEntries(
         glob
-          .sync('pages/**/*.html')
+          .sync('page/**/*.html')
           .map((file) => [
             path.relative('pages', file.slice(0, file.length - path.extname(file).length)),
             fileURLToPath(new URL(file, import.meta.url)),
@@ -72,6 +47,5 @@ export default defineConfig({
       ),
     },
     outDir: 'dist',
-    assetsDir: 'assets',
   },
 });
