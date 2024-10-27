5306d6bbfe4700d075e13c52999879ca5fbdadaa
CubeAndy <a072682@gmail.com>
2024-10-27 19:25:24 +0800

    1027更新-2

--- main.js
+++ main.js
@@ -2,14 +2,4 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
-import { Modal } from 'bootstrap';
 
-const modal1 = new Modal(document.getElementById('exampleModal'));
-document.getElementById('submitBtn').addEventListener('click', () => {
-  modal1.show();
-});
-
-const modal2 = new Modal(document.getElementById('bb'));
-document.getElementById('aa').addEventListener('click', () => {
-  modal2.show();
-});
