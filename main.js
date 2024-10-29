c58d149323823f99f4c6f0aeb639613b67d3ce78
CubeAndy <a072682@gmail.com>
2024-10-30 06:08:56 +0800

    1030更新 檔案格式跳窗說明 完成 送出留言時的彈跳視窗樣式編輯 完成 nav選單手機模式下 新手指南按鈕失效 完成 nav選單手機模式下 樣式修改 完成 登錄系統 完成 登錄系統文字驗證 完成

--- main.js
+++ main.js
@@ -6,31 +6,49 @@ console.log("Hello world!");
 
 // 登入功能
 const loginForm = document.getElementById('loginForm');
-const errorMessage = document.getElementById('errorMessage');
-const profilePic = document.getElementById('aa');
-console.log("測試一下");
+const logoutForm = document.getElementById('logoutForm');
+const errorMessage1 = document.getElementById('errorMessage1');
+const errorMessage2 = document.getElementById('errorMessage2');
+const navbarLog1 = document.getElementById('navbar-log1');
+const navbarLog2 = document.getElementById('navbar-log2');
 
 loginForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    
-    const username = document.getElementById('username').value;
+    console.log("我有點到!");
+    const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
-    
+
+    // 隱藏錯誤訊息初始設置
+    errorMessage1.style.display = 'none';
+    errorMessage2.style.display = 'none';
+
     // 驗證帳號密碼
-    if (username === '123' && password === '123') {
+    if (email === "" || password === "") {
+        console.log("我是空的");
+        errorMessage1.style.display = 'block';
+    } else if (email === '123@123' && password === '123') {
+        console.log("我是對的!!!");
         // 登入成功，變更頭像
-        profilePic.src = 'https://fakeimg.pl/80x80/';  // 使用者頭像
-        errorMessage.style.display = 'none';
+        navbarLog1.style.display = 'none';
+        navbarLog2.style.display = 'block';
         
         // 隱藏彈出視窗
-        const loginModalInstance = Modal.getInstance(document.getElementById("bb"));
-        
-        
-         loginModalInstance.hide();
-        
-
+        const loginModalInstance = Modal.getInstance(document.getElementById("login-modal"));
+        loginModalInstance.hide();
     } else {
         // 顯示錯誤訊息
-        errorMessage.style.display = 'block';
-    }
-});
+        errorMessage2.style.display = 'block';
+        console.log("我是錯的!!!");
+    }});
+
+logoutForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    
+        navbarLog1.style.display = 'block';
+        navbarLog2.style.display = 'none';
+        const loginModalInstance = Modal.getInstance(document.getElementById("logout-modal"));
+        loginModalInstance.hide();
+        
+        
+    });
